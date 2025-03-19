0aa58dfbf9f22d7190ca1dc1591b98f05906625b
Marta <Marta@DESKTOP-9D5G6UC>
2025-03-19 17:47:39 +0100

    Revert "Mi proyecto FriasGutierrezMarta2425T4 comentado con javadoc"
    This reverts commit 83e835671a948f12b16517cae693d031faefcc64.

--- src/libreria/LibreriaFriasGutierrezMarta2425T4.java
+++ src/libreria/LibreriaFriasGutierrezMarta2425T4.java
@@ -1,164 +1,113 @@
+
 package libreria;
 
-/**
- * Clase que representa la disponibilidad (stock) de libros, sus precio y el saldo de una libreria.
- * @author Marta Frías Gutiérrez
- * 
- */
 public class LibreriaFriasGutierrezMarta2425T4 {
-
-  private String nom;      // Nombre del libro
-  private int cantidad;     // Cantidad de ejemplares en stock
-  private double precio;  // Precio unitario del libro
-  private double saldo;   // Saldo de la librería
-
+     private String nom;
+    private int cantidad;
+    private double precio;
+    private double saldo;
+    
   /**
-   * Método que obtiene el nombre del libro
-   * @return Nombre del libro
+   * @return the nom
    */
   public String getNom() {
     return nom;
   }
 
   /**
-   * Método que establece el nombre del libro
-   * @param nom Nombre del libro a establecer
+   * @param nom the nom to set
    */
   public void setNom(String nom) {
     this.nom = nom;
   }
 
   /**
-   * Método que obtiene la cantidad de libros disponibles (en stock)
-   * @return Cantidad de libros disponibles (en stock)
+   * @return the cantidad
    */
   public int getCantidad() {
     return cantidad;
   }
 
   /**
-   * Método que establece la cantidad de libros disponibles (en stock)
-   * @param cantidad Nueva cantidad de libros disponibles (en stock)
+   * @param cantidad the cantidad to set
    */
   public void setCantidad(int cantidad) {
     this.cantidad = cantidad;
   }
 
   /**
-   * Método que obtiene el precio de un libro
-   * @return Precio del libro
+   * @return the precio
    */
   public double getPrecio() {
     return precio;
   }
 
   /**
-   * Método que establece el precio de un libro
-   * @param precio Nuevo precio del libro
+   * @param precio the precio to set
    */
   public void setPrecio(double precio) {
     this.precio = precio;
   }
 
   /**
-   * Método que obtiene el saldo de la librería
-   * @return Saldo actual
+   * @return the saldo
    */
   public double getSaldo() {
     return saldo;
   }
 
   /**
-   * Método que establece el saldo de la librería
-   * @param saldo Nuevo saldo a establecer
+   * @param saldo the saldo to set
    */
   public void setSaldo(double saldo) {
     this.saldo = saldo;
   }
 
-  /**
-   * Constructor sin parámetros
-   */
-  public LibreriaFriasGutierrezMarta2425T4() {
-  }
-
-  /**
-   * Constructor con cuatro parámetros para inicializar la librería
-   * @param nom Nombre del libro
-   * @param cantidad Cantidad de ejemplares que hay disponibles (en stock)
-   * @param precio Precio del libro
-   * @param saldo Saldo en la cuenta de la librería
-   */
-  public LibreriaFriasGutierrezMarta2425T4(String nom, int cantidad, double precio, double saldo) {
-    this.nom = nom;
-    this.cantidad = cantidad;
-    this.precio = precio;
-    this.saldo = saldo;
-  }
-
-  /**
-   * Método que devuelve el nombre del libro
-   * @return Nombre del libro
-   */
-  public String obtenerNombre() {
-    return getNom();
-  }
-
-  /**
-   * Método que devuelve el precio del libro
-   * @return Precio del libro
-   */
-  public double obtenerPrecio() {
-    return getPrecio();
-  }
-
-  /**
-   * Método que devuelve la cantidad de libros disponibles (en stock)
-   * @return Cantidad de libros disponibles (en stock)
-   */
-  public int obtenerStock() {
-    return getCantidad();
-  }
-
-  /**
-   * Método que devuelve el saldo actual de la librería
-   * @return Saldo de la librería
-   */
-  public double obtenerSaldo() {
-    return getSaldo();
-  }
-
-  /**
-   * Método para aumentar el saldo de la librería.
-   * @param saldomas Cantidad a incrementar en el saldo
-   * @throws Exception Si la cantidad a ingresar es menor o igual a cero
-   */
-  public void aumentarSaldo(double saldomas) throws Exception {
-    if (saldomas <= 0) {
-      throw new Exception("No se puede ingresar 0 o una cantidad negativa");
+    //Constructor sin argumentos
+    public LibreriaFriasGutierrezMarta2425T4(){}
+    //constructor con parámetros para inicializar todas las propiedades de la clase
+    public LibreriaFriasGutierrezMarta2425T4(String nom, int cantidad, double precio, double saldo){
+        this.nom = nom;                 //Nombre del libro
+        this.cantidad = cantidad;       //Cantidad de ejemplares que hay en stock
+        this.precio = precio;           //Precio unitario del libro
+        this.saldo = saldo;             //Saldo en la cuenta de la librería
     }
-    setSaldo(getSaldo() + saldomas);
-  }
-
-  /**
-   * Método para comprar un libro, reduciendo el stock y aumentando el saldo
-   * @param canti Cantidad de libros a comprar
-   * @throws Exception Si la cantidad es negativa, el stock es insuficiente o no hay saldo suficiente para comprar
-   */
-  public void comprarLibro(int canti) throws Exception {
-    if (canti <= 0) {
-      throw new Exception("No se puede retirar una cantidad negativa de libros");
+    //Método que devuelve el nombre del libro
+    public String obtenerNombre(){
+        return getNom();
     }
-    if (getCantidad() >= canti) {
-      if (getSaldo() <= getPrecio()) {
-        throw new Exception("No hay suficiente saldo para comprar");
-      } else {
-        setCantidad(getCantidad() - canti);
-        setSaldo(getSaldo() + (getPrecio() * canti));
-      }
-    } else {
-      throw new Exception("No se pueden comprar más libros de los que hay disponibles");
+    //Método que devuelve el precio del libro
+    public double obtenerPrecio(){
+        return getPrecio();
     }
-
-  }
+    //Método que devuelve la cantidad de libros de ese ejemplar que quedan para vender
+    public int obtenerStock(){
+        return getCantidad();
+    }
+    //Método para obtener el saldo en la cuenta
+    public double obtenerSaldo(){
+        return getSaldo();
+    }
+    //Método para aumentar el saldo
+    public void aumentarSaldo(double saldomas) throws Exception{
+        if (saldomas<=0)
+            throw new Exception("No se puede ingresar 0 o una cantidad negativa");
+        setSaldo(getSaldo() + saldomas);
+    }
+    //Método para comprar un ejemplar de un libro, actualiza el stock y el saldo
+    public void comprarLibro(int canti) throws Exception{
+        if (canti <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa de libros");
+        if (getCantidad()>=canti){
+            if (getSaldo() <= getPrecio())
+                  throw new Exception ("No hay suficiente saldo para comprar");
+            else{
+                setCantidad(getCantidad() - canti);
+                setSaldo(getSaldo() + (getPrecio() * canti));
+            }
+        }
+        else 
+            throw new Exception ("No se pueden comprar más libros de los que hay disponibles");
+       
+    }
 }
--- src/libreria/Main.java
+++ src/libreria/Main.java
@@ -1,85 +1,61 @@
+
 package libreria;
 
-/**
- * Clase principal de nuestra librería. Aquí gestionamos la compra de libros y
- * el saldo disponible.
- */
 public class Main {
+  
+    public static void main(String[] args) {
+        LibreriaFriasGutierrezMarta2425T4 miLibreriaFriasGutierrezMarta2425T4;
+        String x;
+        int stock;
+        int num;
+        double dinero;
+        double saldoActual;
+        double ingreso;
+        
+        miLibreriaFriasGutierrezMarta2425T4 = new LibreriaFriasGutierrezMarta2425T4("Don Quijote de la Mancha", 10, 45, 120);
 
-  /**
-   * Método principal de la aplicación
-   *
-   * @param args Argumentos de línea de comandos
-   */
-  public static void main(String[] args) {
-    LibreriaFriasGutierrezMarta2425T4 miLibreriaFriasGutierrezMarta2425T4;
-    String x;
-    int stock;
-    int num;
-    double dinero;
-    double saldoActual;
-    double ingreso;
-
-    // Creación de una librería con un libro inicial: Don Quijote de la Mancha
-    miLibreriaFriasGutierrezMarta2425T4 = new LibreriaFriasGutierrezMarta2425T4("Don Quijote de la Mancha", 10, 45, 120);
-
-    // Realizamos la compra de un ejemplar de Don Quijote
     compraQuijoteFriasGutierrezMarta2425T4(miLibreriaFriasGutierrezMarta2425T4);
-
-    // Mostramos el saldo actual después de la compra
-    saldoActual = miLibreriaFriasGutierrezMarta2425T4.obtenerSaldo();
-    System.out.println("El saldo al comprar el libro es de " + saldoActual);
-
-    // Obtenemos el nombre del libro y el stock disponible
-    x = miLibreriaFriasGutierrezMarta2425T4.obtenerNombre();
-    stock = miLibreriaFriasGutierrezMarta2425T4.obtenerStock();
-    System.out.println("El libro " + x + " tiene un stock de " + stock + " unidades ahora");
-
-    // Añadimos un ingreso tras la venta de un libro
+        
+        saldoActual = miLibreriaFriasGutierrezMarta2425T4.obtenerSaldo();
+        System.out.println("El saldo al comprar el libro es de "+ saldoActual );        
+        x = miLibreriaFriasGutierrezMarta2425T4.obtenerNombre();
+        stock = miLibreriaFriasGutierrezMarta2425T4.obtenerStock();
+        System.out.println("El libro "+x+" tiene un stock de "+stock+" unidades ahora");
+        
     añadeIngresoFriasGutierrezMarta2425T4(miLibreriaFriasGutierrezMarta2425T4, "Libro vendido");
-  }
+    }
 
-  /**
-   * Método que añade un ingreso al saldo de la librería.
-   *
-   * @param miLibreriaFriasGutierrezMarta2425T4 Ilibreria sobre la que se
-   * realiza la compra
-   * @param concepto Descripción del ingreso realizado
-   */
   public static void añadeIngresoFriasGutierrezMarta2425T4(LibreriaFriasGutierrezMarta2425T4 miLibreriaFriasGutierrezMarta2425T4, String concepto) {
     double ingreso;
     double saldoActual;
-    try {
+    try
+    {
       ingreso = 30.5; //Ingreso que se va a realizar en el saldo de la librería
       miLibreriaFriasGutierrezMarta2425T4.aumentarSaldo(ingreso);
       saldoActual = miLibreriaFriasGutierrezMarta2425T4.obtenerSaldo();
-      System.out.println("Tu saldo actual después de ingresar es de " + saldoActual + "€");
-    } catch (Exception e) {
+      System.out.println("Tu saldo actual después de ingresar es de "+saldoActual+"€");
+    } catch (Exception e)
+    {
       System.out.println("Fallo al obtener el saldo al ingresar");
     }
   }
 
-  /**
-   * Método que realiza la compra de ejemplares de "Don Quijote de la Mancha" y
-   * actualiza el saldo de la librería
-   *
-   * @param miLibreriaFriasGutierrezMarta2425T4 Ilibreria sobre la que se
-   * realiza la compra.
-   */
   public static void compraQuijoteFriasGutierrezMarta2425T4(LibreriaFriasGutierrezMarta2425T4 miLibreriaFriasGutierrezMarta2425T4) {
     String x;
     int num;
     double dinero;
     double saldoActual;
-    try {
+    try
+    {
       x = "Don Quijote de la Mancha";
       num = 2;    //Número de ejemplares a comprar
       dinero = miLibreriaFriasGutierrezMarta2425T4.obtenerPrecio();
       saldoActual = miLibreriaFriasGutierrezMarta2425T4.obtenerSaldo();
-      System.out.println("El libro " + x + " vale " + dinero + "€");
-      System.out.println("El saldo actual de la librería es de " + saldoActual + "€");
+      System.out.println("El libro "+x+" vale "+dinero+"€");
+      System.out.println("El saldo actual de la librería es de "+saldoActual+"€");
       miLibreriaFriasGutierrezMarta2425T4.comprarLibro(num);
-    } catch (Exception e) {
+    } catch (Exception e)
+    {
       System.out.println("Error en la compra del libro");
     }
   }
